162247cfcdb7ff40f829b9052b41ce5b2b29cf34
soulboy <soulllboy1@gmail.com>
2021-09-29 20:24:56 +0500

    fixed interactive elements, title, margins

--- script.js
+++ script.js
@@ -9,30 +9,60 @@ var onClickDelete = function () {
 var onClickSave = function () {
   var formCurr = this.parentElement.parentElement;
   var formLegacy = formCurr.nextSibling;
-  formLegacy.querySelector("h1").innerHTML =
-    formCurr.querySelector(".field_title").value;
-  formLegacy.querySelector("h2").innerHTML =
-    formCurr.querySelector(".field_author").value;
-  let dateX = formLegacy.querySelector("h3").innerHTML;
-  let editX = " (edited " + new Date().toLocaleString() + ")";
-  formLegacy.querySelector("h3").innerHTML =
-    dateX.indexOf("edited") > -1 ? dateX.slice(0, -30) + editX : dateX + editX;
-  formLegacy.querySelector(".paragraphs_part").innerHTML =
-    formCurr.querySelector(".field_text").value;
-  formLegacy.style.display = "flex";
-  formCurr.remove();
+  var boolChecker = true;
+  var field01 = formCurr.querySelector(".field_title");
+  if (field01.value == "") {
+    boolChecker = false;
+    field01.style.border = "1px solid red";
+  } else {
+    field01.style.border = "1px solid #d9d9d9";
+  }
+  var field02 = formCurr.querySelector(".field_author");
+  if (field02.value == "") {
+    boolChecker = false;
+    field02.style.border = "1px solid red";
+  } else {
+    field02.style.border = "1px solid #d9d9d9";
+  }
+  var field03 = formCurr.querySelector(".field_text");
+  if (field03.value == "") {
+    boolChecker = false;
+    field03.style.border = "1px solid red";
+  } else {
+    field03.style.border = "1px solid #d9d9d9";
+  }
+  if (boolChecker == true) {
+    formLegacy.querySelector("h1").innerHTML = field01.value;
+    formLegacy.querySelector("h2").innerHTML = field02.value;
+    let dateX = formLegacy.querySelector("h3").innerHTML;
+    let editX = " (edited " + new Date().toLocaleString() + ")";
+    formLegacy.querySelector("h3").innerHTML =
+      dateX.indexOf("edited") > -1
+        ? dateX.slice(0, -30) + editX
+        : dateX + editX;
+    formLegacy.querySelector(".paragraphs_part").innerHTML = field03.value;
+    formLegacy.style.display = "flex";
+    formCurr.remove();
+  }
 };
 
 var onClickEdit = function () {
   var formLegacy = this.parentElement.parentElement;
   var form2 = document.querySelector(".form_edit_post").cloneNode(true);
   form2.style.display = "flex";
-  form2.querySelector(".field_title").value =
-    formLegacy.querySelector("h1").innerHTML;
-  form2.querySelector(".field_author").value =
-    formLegacy.querySelector("h2").innerHTML;
-  form2.querySelector(".field_text").value =
-    formLegacy.querySelector(".paragraphs_part").innerHTML;
+  form2.querySelector(".field_title").value = formLegacy
+    .querySelector("h1")
+    .textContent.replace(/\s+/g, " ")
+    .trim();
+  form2.querySelector(".field_author").value = formLegacy
+    .querySelector("h2")
+    .textContent.replace(/\s+/g, " ")
+    .trim();
+  form2.querySelector(".field_text").value = formLegacy
+    .querySelector(".paragraphs_part")
+    .textContent.replace(/\s+/g, " ")
+    .trim();
+  form2.querySelector(".field_text").style.height = "150px";
   form2
     .querySelectorAll(".btn_default")[0]
     .addEventListener("click", onClickSave);
@@ -43,20 +73,55 @@ var onClickEdit = function () {
 var onClickAdd = function () {
   var formLegacy = btnAddEl.parentElement.parentElement;
   var form2 = document.querySelector(".form_post").cloneNode(true);
-  form2.querySelector("h1").innerHTML =
-    formLegacy.querySelector(".field_title").value;
-  form2.querySelector("h2").innerHTML =
-    formLegacy.querySelector(".field_author").value;
-  form2.querySelector("h3").innerHTML = new Date().toLocaleString();
-  form2.querySelector(".paragraphs_part").innerHTML =
-    formLegacy.querySelector(".field_text").value;
-  form2
-    .querySelectorAll(".btn_default")[0]
-    .addEventListener("click", onClickEdit);
-  form2
-    .querySelectorAll(".btn_default")[1]
-    .addEventListener("click", onClickDelete);
-  sep_or.before(form2);
+  var boolChecker = true;
+  var field01 = formLegacy.querySelector(".field_title");
+  if (field01.value == "") {
+    boolChecker = false;
+    field01.style.border = "1px solid red";
+  } else {
+    field01.style.border = "1px solid #d9d9d9";
+  }
+  var field02 = formLegacy.querySelector(".field_author");
+  if (field02.value == "") {
+    boolChecker = false;
+    field02.style.border = "1px solid red";
+  } else {
+    field02.style.border = "1px solid #d9d9d9";
+  }
+  var field03 = formLegacy.querySelector(".field_text");
+  if (field03.value == "") {
+    boolChecker = false;
+    field03.style.border = "1px solid red";
+  } else {
+    field03.style.border = "1px solid #d9d9d9";
+  }
+  if (boolChecker == true) {
+    form2.querySelector("h1").innerHTML = field01.value;
+    field01.value = "";
+    form2.querySelector("h2").innerHTML = field02.value;
+    field02.value = "";
+    form2.querySelector("h3").innerHTML = new Date().toLocaleString();
+    form2.querySelector(".paragraphs_part").innerHTML = field03.value;
+    field03.value = "";
+    form2
+      .querySelectorAll(".btn_default")[0]
+      .addEventListener("click", onClickEdit);
+    form2
+      .querySelectorAll(".btn_default")[1]
+      .addEventListener("click", onClickDelete);
+    sep_or.before(form2);
+  }
 };
 
 btnAddEl.addEventListener("click", onClickAdd);
+
+var btnsEdit = document.querySelectorAll(
+  ".form_post .form_footer .btn_default:first-child"
+);
+var btnsDelete = document.querySelectorAll(
+  ".form_post .form_footer .btn_default:last-child"
+);
+for (var i = 0; i < 3; i += 1) {
+  btnsEdit[i].addEventListener("click", onClickEdit);
+  btnsDelete[i].addEventListener("click", onClickDelete);
+}
